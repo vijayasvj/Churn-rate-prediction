d55abdafa385dcb79e69e299f4a746cf98a8ab2a
Vijay Murugadoss <65366176+vijayasvj@users.noreply.github.com>
2024-06-03 16:22:29 +0530

    Delete Telco-Customer-Churn-Prediction-Streamlit-App directory

--- Telco-Customer-Churn-Prediction-Streamlit-App/test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyA1Kv4IFPrh6u60mnLfZgKfZlRQk0K3Q3A")
-# The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-model = genai.GenerativeModel('gemini-1.5-flash')
-response = model.generate_content("Write a story about a magic backpack.")
-print(response.text)
